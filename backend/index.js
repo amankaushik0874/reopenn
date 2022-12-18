4e8daf5ff8165fa4e3301a854870131c4e13c350
Aman Kaushik <aman.kaushik@sof.to>
2022-12-19 01:57:40 +0530

    header change

--- backend/index.js
+++ backend/index.js
@@ -6,27 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Enable CORS for the https://reopen-front.netlify.app/ domain
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-
-  // if (req.method === "OPTIONS") {
-  //   res.status(200).end();
-  //   return;
-  // }
-
-  next();
-});
+app.use(cors({ origin: "https://reopen-front.netlify.app/" }));
 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
